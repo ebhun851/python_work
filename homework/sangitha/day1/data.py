81d0fbc73b3555adbb163c9d3d76c2f32765226e
sangeetha jillela <sangeethajillela@sangeethas-MacBook-Pro.local>
2017-09-12 22:22:13 -0700

    added files related to day-1 homework

---
+++ homework/sangitha/day1/data.py
@@ -0,0 +1,14 @@
+'''
+Created on Sep 9, 2017
+
+@author: sangeethajillela
+'''
+s="my name is sangeetha and neelesh"
+a=s.__len__() -1
+print s[0],s[a]
+
+print s.__len__()
+b="this is nivedh"
+c="this is jagjeeth"
+print(b+c)
+print b[1:b.__len__()-1]
